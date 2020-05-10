6de94758f6fcfaabfbc42e860ef9fe8b061ba992
Roman Garanin <git@romangaranin.net>
2020-05-10 14:00:30 +0300

    feat: day 10, simple approach

---
+++ 2020-may-challenge/10_findTheTownJudge.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	assert(findJudge(2, [][]int{{1, 2}}), 2)
+	assert(findJudge(3, [][]int{{1, 3}, {2, 3}}), 3)
+	assert(findJudge(3, [][]int{{1, 3}, {2, 3}, {3, 1}}), -1)
+	assert(findJudge(3, [][]int{{1, 2}, {2, 3}}), -1)
+	assert(findJudge(4, [][]int{{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}}), 3)
+	assert(findJudge(1, [][]int{}), 1)
+}
+
+func assert(got, want interface{}) {
+	fmt.Printf("got: %t, want: %t\n", got, want)
+}
+
+func findJudge(N int, trust [][]int) int {
+	trustedBy := make([][]int, N) //alternatively: keep just count, not values
+	trustsTo := make([][]int, N)
+
+	for _, trustEntry := range trust {
+		who := trustEntry[0] - 1
+		to := trustEntry[1] - 1
+
+		trustedBy[to] = append(trustedBy[to], who)
+		trustsTo[who] = append(trustedBy[who], to)
+	}
+
+	for who, trustedByList := range trustedBy {
+		if len(trustedByList) == N-1 && len(trustsTo[who]) == 0 {
+			return who + 1
+		}
+	}
+
+	return -1
+}
