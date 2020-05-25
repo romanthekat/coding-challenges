66f7b3c001bf148e6ccbda2fe25d06115aacbfff
Roman Garanin <git@romangaranin.net>
2020-05-25 21:08:08 +0300

    feat: day 25, tabulation lcs

--- 2020-may-challenge/25_uncrossedLines.go
+++ 2020-may-challenge/25_uncrossedLines.go
@@ -9,6 +9,8 @@ func main() {
 	assert(maxUncrossedLines([]int{1, 4, 2}, []int{1, 2, 4}), 2)
 	assert(maxUncrossedLines([]int{2, 5, 1, 2, 5}, []int{10, 5, 2, 1, 5, 2}), 3)
 	assert(maxUncrossedLines([]int{1, 3, 7, 1, 7, 5}, []int{1, 9, 2, 5, 1}), 2)
+	assert(maxUncrossedLines([]int{3, 1, 2, 1, 4, 1, 2, 2, 5, 3, 2, 1, 1, 4, 5, 2, 3, 2, 5, 5},
+		[]int{2, 4, 1, 2, 3, 4, 2, 4, 5, 5, 1, 1, 2, 1, 1, 1, 5, 4, 1, 4, 2, 1, 5, 4, 2, 3, 1, 5, 2, 1}), 14)
 }
 
 func assert(got, want interface{}) {
@@ -16,17 +18,25 @@ func assert(got, want interface{}) {
 }
 
 func maxUncrossedLines(a []int, b []int) int {
-	return lcs(a, b, len(a), len(b))
-}
-
-func lcs(a []int, b []int, i, j int) int {
-	if i == 0 || j == 0 {
-		return 0
-	} else if a[i-1] == b[j-1] {
-		return 1 + lcs(a, b, i-1, j-1)
-	} else {
-		return max(lcs(a, b, i, j-1), lcs(a, b, i-1, j))
+	table := make([][]int, len(a)+1)
+	for i := 0; i <= len(a); i++ {
+		table[i] = make([]int, len(b)+1)
 	}
+
+	for i := 0; i <= len(a); i++ {
+		for j := 0; j <= len(b); j++ {
+			if i == 0 || j == 0 {
+				table[i][j] = 0
+			} else if a[i-1] == b[j-1] {
+				table[i][j] = table[i-1][j-1] + 1
+			} else {
+				table[i][j] = max(table[i][j-1], table[i-1][j])
+			}
+
+		}
+	}
+
+	return table[len(a)][len(b)]
 }
 
 func max(i, j int) int {
