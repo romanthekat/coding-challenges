86184637698005682f5f5a1d833f6f124b4e52c2
Roman Garanin <git@romangaranin.net>
2020-06-21 19:33:19 +0300

    feat: permutations, backtracking algorithm

---
+++ java/src/main/java/dev/romangaranin/leetcode/Permutations.java
@@ -0,0 +1,50 @@
+package dev.romangaranin.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a collection of distinct integers, return all possible permutations.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [1,2,3]
+ * Output:
+ * [
+ * [1,2,3],
+ * [1,3,2],
+ * [2,1,3],
+ * [2,3,1],
+ * [3,1,2],
+ * [3,2,1]
+ * ]
+ */
+public class Permutations {
+    public static void main(String[] args) {
+        System.out.println(new Solution().permute(new int[]{1, 2, 3}));
+    }
+
+    static class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+            var result = new ArrayList<List<Integer>>();
+            permute(result, nums, new ArrayList<>());
+            return result;
+        }
+
+        protected void permute(List<List<Integer>> result, int[] nums, List<Integer> state) {
+            if (state.size() == nums.length) {
+                result.add(new ArrayList<>(state));
+            } else {
+                for (int num : nums) {
+                    if (state.contains(num)) {
+                        continue;
+                    }
+
+                    state.add(num);
+                    permute(result, nums, state);
+                    state.remove(state.size() - 1);
+                }
+            }
+        }
+    }
+}
