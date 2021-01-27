95b167714ff981c78aaac985a20e2a6cb5eccf1c
Roman Garanin <git@romangaranin.net>
2021-01-27 21:51:53 +0300

    feat: 143 reorder list, empty and nil input cases

--- LeetCode/golang/143_reorderList.go
+++ LeetCode/golang/143_reorderList.go
@@ -24,6 +24,10 @@ Example 2:
 Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
 */
 func reorderList(head *ListNode) {
+	if head == nil {
+		return
+	}
+
 	slow, fast := head, head
 
 	//find mid
@@ -56,6 +60,9 @@ func reorderList(head *ListNode) {
 }
 
 func main() {
+	reorderList(&ListNode{})
+	reorderList(nil)
+
 	node1 := ListNode{Val: 1}
 	node2 := ListNode{Val: 2}
 	node3 := ListNode{Val: 3}
