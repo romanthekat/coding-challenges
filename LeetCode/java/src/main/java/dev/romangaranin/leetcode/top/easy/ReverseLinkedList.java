0e6b55126a85d052ad00cb090dfd6de324d44d45
Roman Garanin <git@romangaranin.net>
2021-09-15 10:28:47 +0300

    feat: Reverse linked list, naive 3 pointers

---
+++ LeetCode/java/src/main/java/dev/romangaranin/leetcode/top/easy/ReverseLinkedList.java
@@ -0,0 +1,98 @@
+package dev.romangaranin.leetcode.top.easy;
+
+import static dev.romangaranin.leetcode.Helper.test;
+
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ */
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        var s = new Solution();
+
+        var input0 = newLinkedList();
+        var output0 = s.reverseList(input0);
+        test(output0, null);
+        System.out.println();
+
+        var input1 = newLinkedList(1);
+        var output1 = s.reverseList(input1);
+        test(output1.val, 1);
+        test(output1.next, null);
+        System.out.println();
+
+        var input2 = newLinkedList(1, 2);
+        var output2 = s.reverseList(input2);
+        test(output2.val, 2);
+        test(output2.next.val, 1);
+        test(output2.next.next, null);
+        System.out.println();
+
+        var input5 = newLinkedList(1, 2, 3, 4, 5);
+        var output5 = s.reverseList(input5);
+        test(output5.val, 5);
+        test(output5.next.val, 4);
+        test(output5.next.next.val, 3);
+        test(output5.next.next.next.val, 2);
+        test(output5.next.next.next.next.val, 1);
+        test(output5.next.next.next.next.next, null);
+    }
+
+    private static ListNode newLinkedList(int... input) {
+        var pseudoHead = new ListNode();
+
+        var current = pseudoHead;
+        for (var value : input) {
+            current.next = new ListNode(value);
+            current = current.next;
+        }
+
+        return pseudoHead.next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    '}';
+        }
+    }
+
+    static class Solution {
+        public ListNode reverseList(ListNode head) {
+            if (head == null) {
+                return head;
+            }
+
+            var prev = head;
+            var curr = head.next;
+
+            while (curr != null) {
+                var next = curr.next;
+                curr.next = prev;
+
+                prev = curr;
+                curr = next;
+            }
+
+            head.next = null;
+
+            return prev;
+        }
+    }
+}
