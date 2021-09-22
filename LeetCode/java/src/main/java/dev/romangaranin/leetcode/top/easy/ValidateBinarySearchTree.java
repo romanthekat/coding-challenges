40108d25507aa63d64bcc0835c9be588fd934bb6
Roman Garanin <git@romangaranin.net>
2021-09-22 22:18:07 +0300

    feat: validate bst, better cases naming

--- LeetCode/java/src/main/java/dev/romangaranin/leetcode/top/easy/ValidateBinarySearchTree.java
+++ LeetCode/java/src/main/java/dev/romangaranin/leetcode/top/easy/ValidateBinarySearchTree.java
@@ -21,30 +21,18 @@ public class ValidateBinarySearchTree {
     public static void main(String[] args) {
         var s = new Solution();
 
-        var node3 = new TreeNode(3);
-        var node9 = new TreeNode(9);
-        var node20 = new TreeNode(20);
-        var node15 = new TreeNode(15);
-        var node7 = new TreeNode(7);
-        node3.left = node9;
-        node3.right = node20;
-        node20.left = node15;
-        node20.right = node7;
+        var case1 = new TreeNode(10, new TreeNode(11), new TreeNode(9));
+        test(s.isValidBST(case1), false);
 
-        test(s.isValidBST(node3), false);
-
-        var node10 = new TreeNode(10, new TreeNode(11), new TreeNode(9));
-        test(s.isValidBST(node10), false);
-
-        var node128 = new TreeNode(128,
+        var case2 = new TreeNode(128,
                 new TreeNode(64, new TreeNode(32), new TreeNode(72)),
                 new TreeNode(256, null, new TreeNode(512)));
-        test(s.isValidBST(node128), true);
+        test(s.isValidBST(case2), true);
 
-        var case4 = new TreeNode(5,
+        var case3 = new TreeNode(5,
                 new TreeNode(4),
                 new TreeNode(6, new TreeNode(3), new TreeNode(7)));
-        test(s.isValidBST(case4), false);
+        test(s.isValidBST(case3), false);
 
         test(s.isValidBST(new TreeNode(2147483647)), true);
     }
