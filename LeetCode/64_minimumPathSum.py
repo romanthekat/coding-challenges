4807e580786563eb12d1a69596598505ba7ec709
Roman Garanin <git@romangaranin.net>
2022-03-27 21:30:53 +0300

    feat: 64 min path sum, dp with 2d array, simpler

--- LeetCode/64_minimumPathSum.py
+++ LeetCode/64_minimumPathSum.py
@@ -16,32 +16,29 @@ Example 2:
 Input: grid = [[1,2,3],[4,5,6]]
 Output: 12
 """
-import sys
 from typing import List
 from common import assert_equal
 
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        paths = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        max_y = len(grid)
+        max_x = len(grid[0])
 
-        for i in range(0, len(grid)):
-            for j in range(0, len(grid[0])):
-                if i == 0 and j == 0:
-                    paths[0][0] = grid[0][0]
-                    continue
+        paths = [[0 for _ in range(max_x)] for _ in range(max_y)]
+        paths[0][0] = grid[0][0]
 
-                up = self.get_path(paths, i, j - 1)
-                left = self.get_path(paths, i - 1, j)
-                paths[i][j] = grid[i][j] + min(left, up)
+        for i in range(1, max_y):
+            paths[i][0] = grid[i][0] + paths[i - 1][0]
+
+        for j in range(1, max_x):
+            paths[0][j] = grid[0][j] + paths[0][j - 1]
+
+        for i in range(1, max_y):
+            for j in range(1, max_x):
+                paths[i][j] = grid[i][j] + min(paths[i][j - 1], paths[i - 1][j])
 
         return paths[-1][-1]
-
-    def get_path(self, paths, x, y) -> int:
-        if x < 0 or x >= len(paths) or y < 0 or y >= len(paths[0]):
-            return sys.maxsize
-
-        return paths[x][y]
 
 
 if __name__ == '__main__':
