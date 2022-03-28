5d3cace64e1f9343036db0a52fe904f7b69efd4d
Roman Garanin <git@romangaranin.net>
2022-03-28 22:52:46 +0300

    feat: 64 min path sum, docs

--- LeetCode/64_minimumPathSum.go
+++ LeetCode/64_minimumPathSum.go
@@ -9,19 +9,18 @@ Given a m x n grid filled with non-negative numbers, find a path from top left t
 
 Note: You can only move either down or right at any point in time.
 
-
-
 Example 1:
-
+```
 Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
 Output: 7
 Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+```
 
 Example 2:
-
+```
 Input: grid = [[1,2,3],[4,5,6]]
 Output: 12
-"""
+```
 */
 func main() {
 	common.AssertEqual(minPathSum([][]int{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}), 7)
