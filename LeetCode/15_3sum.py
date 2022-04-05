ecd366fb1ad07745f4ee6f59d117f9a5b86f0158
Roman Garanin <git@romangaranin.net>
2022-04-05 11:04:17 +0300

    feat: 15 3 sum, set approach

--- LeetCode/15_3sum.py
+++ LeetCode/15_3sum.py
@@ -25,7 +25,35 @@ from typing import List
 from common import assert_equal
 
 
-class Solution:
+class SolutionSet:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        nums = sorted(nums)
+
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+
+            if i != 0 and nums[i] == nums[i - 1]:
+                continue
+
+            seen = set()
+            j = i + 1
+            while j < len(nums):
+                target_num = - nums[i] - nums[j]
+                if target_num in seen:
+                    result.append([nums[i], nums[j], target_num])
+                    while j + 1 < len(nums) and nums[j] == nums[j + 1]:
+                        j += 1
+
+                seen.add(nums[j])
+                j += 1
+
+        return result
+
+
+class SolutionTwoPointers:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = []
 
@@ -59,7 +87,7 @@ class Solution:
 
 
 if __name__ == '__main__':
-    s = Solution()
+    s = SolutionTwoPointers()
 
     assert_equal(s.threeSum([-1, 0, 1, 2, -1, -4]), [[-1, -1, 2], [-1, 0, 1]])
     assert_equal(s.threeSum([0, 0, 0]), [[0, 0, 0]])
