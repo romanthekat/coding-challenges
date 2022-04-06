584bc86f88a84389adeea4362d0a627aec95dbf0
Roman Garanin <git@romangaranin.net>
2022-04-06 09:46:32 +0300

    feat: 20 valid parentheses, stack

---
+++ LeetCode/20_validParentheses.py
@@ -0,0 +1,59 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+
+
+Constraints:
+
+    1 <= s.length <= 10^4
+    s consists of parentheses only '()[]{}'.
+
+
+"""
+import common
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {"(": ")", "[": "]", "{": "}"}
+        stack = []
+
+        for char in s:
+            if char in brackets:
+                stack.append(char)
+            else:
+                bracket = stack.pop() if stack else "-"
+                if brackets.get(bracket) != char:
+                    return False
+
+        return len(stack) == 0
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    common.assert_equal(s.isValid("()"), True)
+    common.assert_equal(s.isValid("()[]{}"), True)
+    common.assert_equal(s.isValid("(]"), False)
+    common.assert_equal(s.isValid("["), False)
+    common.assert_equal(s.isValid("]"), False)
