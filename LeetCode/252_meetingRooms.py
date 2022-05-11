20dba1339fd3733a6734a8e08f9bce6322e25a7e
Roman Garanin <git@romangaranin.net>
2022-05-11 10:39:41 +0300

    feat: 252 meeting rooms, simpler iteration

--- LeetCode/252_meetingRooms.py
+++ LeetCode/252_meetingRooms.py
@@ -22,6 +22,17 @@ import common
 
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort(key=lambda x: x[0])
+
+        for i in range(len(intervals) - 1):
+            if intervals[i][1] > intervals[i+1][0]:
+                return False
+
+        return True
+
+
+class SolutionOrderingAndRooms:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         starts = []
         ends = []
 
